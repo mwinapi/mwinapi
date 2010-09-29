9320bb7eba0a05d89e198183135e027a7cbc0c05
Michael Schierl <schierlm@gmx.de>
2010-09-29 16:10:36 +0000

    Fix some problems when using Accessibility with Internet Explorer 8, whose implementation of IAccessible.get_accChild() incorrectly returns InvalidCastException instead of ArgumentException.
    [submitted by Peter Ziewer <ziewer at in dot tum dot de>]
    
    git-svn-id: https://svn.code.sf.net/p/mwinapi/code/trunk@94 fd7b8004-de1b-0410-9a54-a0c85bd34932

--- ManagedWinapi/SystemAccessibleObject.cs
+++ ManagedWinapi/SystemAccessibleObject.cs
@@ -69,6 +69,7 @@ namespace ManagedWinapi.Accessibility
                     }
                 }
                 catch (ArgumentException) { }
+                catch (InvalidCastException) { }
             }
             this.iacc = iacc;
             this.childID = childID;
