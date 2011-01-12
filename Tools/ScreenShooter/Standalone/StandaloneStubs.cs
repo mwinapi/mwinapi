99dad7692001251eb0a6b9f56b77475fe984ee59
Michael Schierl <schierlm@gmx.de>
2011-01-12 22:47:10 +0000

    Let's celebrate Revision 100 with the addition of a standalone version of ScreenShooter (which will run without requiring ManagedWinapi.dll)
    git-svn-id: https://svn.code.sf.net/p/mwinapi/code/trunk@100 fd7b8004-de1b-0410-9a54-a0c85bd34932

---
+++ Tools/ScreenShooter/Standalone/StandaloneStubs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ManagedWinapi.Windows
+{
+    namespace Contents
+    {
+        internal static class WindowContentParser
+        {
+            internal static WindowContent Parse(SystemWindow sw)
+            {
+                throw new NotImplementedException();
+            }
+
+            internal static WindowContent ParsePreview(SystemWindow sw)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public interface WindowContent { }
+    }
+}
