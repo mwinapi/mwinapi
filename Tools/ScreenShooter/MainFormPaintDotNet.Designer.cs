a74ee8ca41514c0fbfd9a77b32528710e86f8b7e
Michael Schierl <schierlm@gmx.de>
2011-01-23 15:43:58 +0000

    Add a standalone ScreenShooter project that starts Paint.NET automatically, as discussed in <http://forums.getpaint.net/index.php?/topic/20860->
    git-svn-id: https://svn.code.sf.net/p/mwinapi/code/trunk@105 fd7b8004-de1b-0410-9a54-a0c85bd34932

---
+++ Tools/ScreenShooter/MainFormPaintDotNet.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace ScreenShooter
+{
+    partial class MainFormPaintDotNet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.ContextMenuStrip trayMenu;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainFormPaintDotNet));
+            this.configureToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.trayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.hideButton = new System.Windows.Forms.Button();
+            this.settings = new ScreenShooter.ScreenshotSettings();
+            trayMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            trayMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // trayMenu
+            // 
+            trayMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.configureToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.quitToolStripMenuItem});
+            trayMenu.Name = "trayMenu";
+            trayMenu.Size = new System.Drawing.Size(140, 54);
+            // 
+            // configureToolStripMenuItem
+            // 
+            this.configureToolStripMenuItem.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold);
+            this.configureToolStripMenuItem.Name = "configureToolStripMenuItem";
+            this.configureToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.configureToolStripMenuItem.Text = "&Configure";
+            this.configureToolStripMenuItem.Click += new System.EventHandler(this.configureToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(136, 6);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.quitToolStripMenuItem.Text = "&Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // trayIcon
+            // 
+            this.trayIcon.ContextMenuStrip = trayMenu;
+            this.trayIcon.Text = "ScreenShooter Paint.NET Edition";
+            this.trayIcon.Visible = true;
+            this.trayIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.trayIcon_MouseDoubleClick);
+            // 
+            // hideButton
+            // 
+            this.hideButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.hideButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.hideButton.Location = new System.Drawing.Point(301, 34);
+            this.hideButton.Name = "hideButton";
+            this.hideButton.Size = new System.Drawing.Size(75, 23);
+            this.hideButton.TabIndex = 0;
+            this.hideButton.Text = "Hide";
+            this.hideButton.UseVisualStyleBackColor = true;
+            this.hideButton.Click += new System.EventHandler(this.hideButton_Click);
+            // 
+            // settings
+            // 
+            this.settings.Location = new System.Drawing.Point(12, 12);
+            this.settings.Name = "settings";
+            this.settings.Size = new System.Drawing.Size(376, 304);
+            this.settings.TabIndex = 1;
+            this.settings.ScreenshotTaken += new ScreenShooter.ScreenshotSettings.ScreenshotHandler(this.settings_ScreenshotTaken);
+            // 
+            // MainFormPaintDotNet
+            // 
+            this.AcceptButton = this.hideButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.hideButton;
+            this.ClientSize = new System.Drawing.Size(400, 328);
+            this.Controls.Add(this.hideButton);
+            this.Controls.Add(this.settings);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainFormPaintDotNet";
+            this.Text = "ScreenShooter Paint.NET Edition";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainFormPaintDotNet_FormClosing);
+            trayMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon trayIcon;
+        private System.Windows.Forms.ToolStripMenuItem configureToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.Button hideButton;
+        private ScreenshotSettings settings;
+    }
+}
+
---
+++ Tools/ScreenShooter/MainFormPaintDotNet.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace ScreenShooter
+{
+    public partial class MainFormPaintDotNet : Form
+    {
+
+        int counter = 0;
+        string lastScreenshot = null;
+
+        public MainFormPaintDotNet()
+        {
+            InitializeComponent();
+            trayIcon.Icon = this.Icon;
+            string[] args = Environment.GetCommandLineArgs();
+            if (args.Length > 1)
+                settings.Settings = args[1];
+            settings.EnableHotkey();
+        }
+
+        private void settings_ScreenshotTaken(Bitmap bitmap)
+        {
+            MainFormPaintDotNet_FormClosing(this, null);
+            lastScreenshot = Path.Combine(Path.GetTempPath(), "~pdn" + counter + ".png");
+            counter++;
+            bitmap.Save(lastScreenshot, ImageFormat.Png);
+            string path = Registry.GetValue(@"HKEY_LOCAL_MACHINE\Software\Paint.NET", "TARGETDIR", null) as string;
+            if (path == null)
+            {
+                MessageBox.Show("Paint.NET is not installed!");
+            }
+            else
+            {
+                Process.Start(Path.Combine(path, "PaintDotNet.exe"), "untitled:\"" + lastScreenshot+"\"");
+            }
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Dispose();
+        }
+
+        private void configureToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Visible = true;
+        }
+
+        private void trayIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Visible = true;
+        }
+
+        private void hideButton_Click(object sender, EventArgs e)
+        {
+            Keys mask = Keys.Shift | Keys.Control;
+            if ((Control.ModifierKeys & mask) == mask)
+            {
+                Clipboard.Clear();
+                Clipboard.SetText(settings.Settings);
+            }
+            Visible = false;
+        }
+
+        private void MainFormPaintDotNet_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (lastScreenshot != null)
+            {
+                File.Delete(lastScreenshot);
+                lastScreenshot = null;
+            }
+        }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainFormPaintDotNet());
+        }
+    }
+}
