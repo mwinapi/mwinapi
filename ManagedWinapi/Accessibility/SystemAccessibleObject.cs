a663f93550a2cc440a6c5a1bd63701b2178cdb45
Peter Ziewer <ziewer@in.tum.de>
2014-11-25 15:39:15 +0000

    Added enum AccRoles with constants for MSAA accessibility roles from oleacc.h
    git-svn-id: https://svn.code.sf.net/p/mwinapi/code/trunk@120 fd7b8004-de1b-0410-9a54-a0c85bd34932

--- ManagedWinapi/Accessibility/SystemAccessibleObject.cs
+++ ManagedWinapi/Accessibility/SystemAccessibleObject.cs
@@ -710,4 +710,327 @@ namespace ManagedWinapi.Accessibility
         /// </summary>
         OBJID_SOUND = 0xFFFFFFF5
     }
+
+    /// <summary>
+    /// This enumeration lists all kinds of accessible roles as returned by IAccessible::get_accRole()
+    /// Constants for MSAA accessibility roles from oleacc.h
+    /// </summary>
+    public enum AccRoles
+    {
+        /// <summary>
+        /// The object represents an alert or a condition that a user should be notified about. This role is used only for objects that embody an alert but are not associated with another user interface element, such as a message box, graphic, text, or sound.
+        /// </summary>
+        ROLE_SYSTEM_ALERT = 8,
+
+        /// <summary>
+        /// The object represents an animation control whose content changes over time, such as a control that displays a series of bitmap frames. Animation controls are displayed when files are copied or when some other time-consuming task is performed.
+        /// </summary>
+        ROLE_SYSTEM_ANIMATION = 54,
+
+        /// <summary>
+        /// The object represents a main window for an application.
+        /// </summary>
+        ROLE_SYSTEM_APPLICATION = 14,
+
+        /// <summary>
+        /// The object represents a window border. The entire border is represented by a single object rather than by separate objects for each side.
+        /// </summary>
+        ROLE_SYSTEM_BORDER = 19,
+
+        /// <summary>
+        /// The object represents a button that expands a list of items.
+        /// </summary>
+        ROLE_SYSTEM_BUTTONDROPDOWN = 56,
+
+        /// <summary>The object represents a button that expands a grid.
+        /// </summary>
+        ROLE_SYSTEM_BUTTONDROPDOWNGRID = 58,
+
+        /// <summary>
+        /// The object represents a button that expands a menu.
+        /// </summary>
+        ROLE_SYSTEM_BUTTONMENU = 57,
+
+        /// <summary>
+        /// The object represents the system caret.
+        /// </summary>
+        ROLE_SYSTEM_CARET = 7,
+
+        /// <summary>
+        /// The object represents a cell within a table.
+        /// </summary>
+        ROLE_SYSTEM_CELL = 29,
+
+        /// <summary>
+        /// The object represents a cartoon-like graphic object, such as Microsoft Office Assistant, which is displayed to provide help to users of an application.
+        /// </summary>
+        ROLE_SYSTEM_CHARACTER = 32,
+
+        /// <summary>
+        /// The object represents a graphical image used to chart data.
+        /// </summary>
+        ROLE_SYSTEM_CHART = 17,
+
+        /// <summary>
+        /// The object represents a check box control: an option that is selected or cleared independently of other options.
+        /// </summary>
+        ROLE_SYSTEM_CHECKBUTTON = 44,
+
+        /// <summary>
+        /// The object represents a window's client area. Microsoft Active Accessibility uses this role as a default if there is a question about the role of a UI element.
+        /// </summary>
+        ROLE_SYSTEM_CLIENT = 10,
+
+        /// <summary>The object represents a control that displays time.
+        /// </summary>
+        ROLE_SYSTEM_CLOCK = 61,
+
+        /// <summary>
+        /// The object represents a column of cells within a table.
+        /// </summary>
+        ROLE_SYSTEM_COLUMN = 27,
+
+        /// <summary>
+        /// The object represents a column header, providing a visual label for a column in a table.
+        /// </summary>
+        ROLE_SYSTEM_COLUMNHEADER = 25,
+
+        /// <summary>
+        /// The object represents a combo box: an edit control with an associated list box that provides a set of predefined choices.
+        /// </summary>
+        ROLE_SYSTEM_COMBOBOX = 46,
+
+        /// <summary>
+        /// The object represents the system's mouse pointer.
+        /// </summary>
+        ROLE_SYSTEM_CURSOR = 6,
+
+        /// <summary>
+        /// The object represents a graphical image that is used to diagram data.
+        /// </summary>
+        ROLE_SYSTEM_DIAGRAM = 53,
+
+        /// <summary>
+        /// The object represents a dial or knob.
+        /// </summary>
+        ROLE_SYSTEM_DIAL = 49,
+
+        /// <summary>
+        /// The object represents a dialog box or message box.
+        /// </summary>
+        ROLE_SYSTEM_DIALOG = 18,
+
+        /// <summary>
+        /// The object represents a document window. A document window is always contained within an application window. This role applies only to MDI windows and refers to the object that contains the MDI title bar.
+        /// </summary>
+        ROLE_SYSTEM_DOCUMENT = 15,
+
+        /// <summary>
+        /// The object represents the calendar control, SysDateTimePick32. The Microsoft Active Accessibility runtime component uses this role to indicate that either a date or a calendar control has been found.
+        /// </summary>
+        ROLE_SYSTEM_DROPLIST = 47,
+
+        /// <summary>
+        /// The object represents a mathematical equation.
+        /// </summary>
+        ROLE_SYSTEM_EQUATION = 55,
+
+        /// <summary>
+        /// The object represents a picture.
+        /// </summary>
+        ROLE_SYSTEM_GRAPHIC = 40,
+
+        /// <summary>
+        /// The object represents a special mouse pointer that allows a user to manipulate user interface elements such as windows. One example of this involves resizing a window by dragging its lower-right corner.
+        /// </summary>
+        ROLE_SYSTEM_GRIP = 4,
+
+        /// <summary>
+        /// The object logically groups other objects. There is not always a parent-child relationship between the grouping object and the objects it contains.
+        /// </summary>
+        ROLE_SYSTEM_GROUPING = 20,
+
+        /// <summary>
+        /// The object displays a help topic in the form of a tooltip or help balloon.
+        /// </summary>
+        ROLE_SYSTEM_HELPBALLOON = 31,
+
+        /// <summary>
+        /// The object represents a keyboard shortcut field that allows the user to enter a combination or sequence of keystrokes.
+        /// </summary>
+        ROLE_SYSTEM_HOTKEYFIELD = 50,
+
+        /// <summary>
+        /// The object represents an indicator, such as a pointer graphic, that points to the current item.
+        /// </summary>
+        ROLE_SYSTEM_INDICATOR = 39,
+
+        /// <summary>
+        /// The object represents an edit control that is designed for an IP address. The edit control is divided into sections, each for a specific part of the IP address.
+        /// </summary>
+        ROLE_SYSTEM_IPADDRESS = 63, // Not defined in all oleacc.h versions
+
+        /// <summary>The object represents a link to something else. This object might look like text or a graphic, but it acts like a button.
+        /// </summary>
+        ROLE_SYSTEM_LINK = 30,
+
+        /// <summary>
+        /// The object represents a list box, allowing the user to select one or more items.
+        /// </summary>
+        ROLE_SYSTEM_LIST = 33,
+
+        /// <summary>
+        /// The object represents an item in a list box or in the list portion of a combo box, drop-down list box, or drop-down combo box.
+        /// </summary>
+        ROLE_SYSTEM_LISTITEM = 34,
+
+        /// <summary>The object represents the menu bar (positioned beneath the title bar of a window) from which users select menus.
+        /// </summary>
+        ROLE_SYSTEM_MENUBAR = 2,
+
+        /// <summary>
+        /// The object represents a menu item: an menu entry that the user can choose to carry out a command, select an option, or display another menu. Functionally, a menu item is equivalent to a push button, a radio button, a check box, or a menu.
+        /// </summary>
+        ROLE_SYSTEM_MENUITEM = 12,
+
+        /// <summary>
+        /// The object represents a menu: a list of options, each with a specific action. All menu types must have role, including the drop-down menus which are displayed when selected from a menu bar; and shortcut menus, which are displayed by clicking the right mouse button.
+        /// </summary>
+        ROLE_SYSTEM_MENUPOPUP = 11,
+
+        /// <summary>
+        /// The object represents an outline or a tree structure, such as a tree view control, that displays a hierarchical list and allows the user to expand and collapse branches.
+        /// </summary>
+        ROLE_SYSTEM_OUTLINE = 35,
+
+        /// <summary>
+        /// The object represents an item that navigates like an outline item. The UP and DOWN ARROW keys are used to navigate through the outline. However, instead of expanding and collapsing when the LEFT and RIGHT ARROW key is pressed, these menus expand or collapse when the SPACEBAR or ENTER key is pressed and the item has focus. 
+        /// </summary>
+        ROLE_SYSTEM_OUTLINEBUTTON = 64, // Not defined in all oleacc.h versions
+
+        /// <summary>
+        /// The object represents an item in an outline or tree structure.
+        /// </summary>
+        ROLE_SYSTEM_OUTLINEITEM = 36,
+
+        /// <summary>
+        /// The object represents a page tab. The only child of a page tab control is a ROLE_SYSTEM_GROUPING object that has the contents of the associated page.
+        /// </summary>
+        ROLE_SYSTEM_PAGETAB = 37,
+
+        /// <summary>
+        /// The object represents a container of page tab controls.
+        /// </summary>
+        ROLE_SYSTEM_PAGETABLIST = 60,
+
+        /// <summary>
+        /// The object represents a pane within a frame or a document window. Users can navigate between panes and within the contents of the current pane, but cannot navigate between items in different panes. Thus, panes represent a grouping level that is lower than frames or document windows, but higher than individual controls. The user navigates between panes by pressing TAB, F6, or CTRL+TAB, depending on the context.
+        /// </summary>
+        ROLE_SYSTEM_PANE = 16,
+
+        /// <summary>
+        /// The object represents a progress bar, which dynamically shows how much of an operation in progress has completed. This control takes no user input.
+        /// </summary>
+        ROLE_SYSTEM_PROGRESSBAR = 48,
+
+        /// <summary>
+        /// The object represents a progress bar, which dynamically shows how much of an operation in progress has completed. This control takes no user input.
+        /// </summary>
+        ROLE_SYSTEM_PROPERTYPAGE = 38,
+
+        /// <summary>
+        /// The object represents a push-button control.
+        /// </summary>
+        ROLE_SYSTEM_PUSHBUTTON = 43,
+
+        /// <summary>
+        /// The object represents an option button (formerly, a radio button). It is one of a group of mutually exclusive options. All objects that share the same parent and that have this attribute are assumed to be part of a single mutually exclusive group. To divide these objects into separate groups, use ROLE_SYSTEM_GROUPING objects.
+        /// </summary>
+        ROLE_SYSTEM_RADIOBUTTON = 45,
+
+        /// <summary>
+        /// The object represents a row of cells within a table.
+        /// </summary>
+        ROLE_SYSTEM_ROW = 28,
+
+        /// <summary>
+        /// The object represents a row header, which provides a visual label for a table row.
+        /// </summary>
+        ROLE_SYSTEM_ROWHEADER = 26,
+
+        /// <summary>
+        /// The object represents a vertical or horizontal scroll bar, which is part of the client area or is used in a control.
+        /// </summary>
+        ROLE_SYSTEM_SCROLLBAR = 3,
+
+        /// <summary>
+        /// The object is used to visually divide a space into two regions. Examples of separator objects include a separator menu item, and a bar that divides split panes within a window.
+        /// </summary>
+        ROLE_SYSTEM_SEPARATOR = 21,
+
+        /// <summary>
+        /// The object represents a slider, which allows the user to adjust a setting in particular increments between minimum and maximum values.
+        /// </summary>
+        ROLE_SYSTEM_SLIDER = 51,
+
+        /// <summary>
+        /// The object represents a system sound, which is associated with various system events.
+        /// </summary>
+        ROLE_SYSTEM_SOUND = 5,
+
+        /// <summary>
+        /// The object represents a spin box, which is a control that allows the user to increment or decrement the value displayed in a separate "buddy" control that is associated with the spin box.
+        /// </summary>
+        ROLE_SYSTEM_SPINBUTTON = 52,
+
+        /// <summary>
+        /// The object represents a button on a toolbar that has a drop-down list icon that is directly adjacent to the button.
+        /// </summary>
+        ROLE_SYSTEM_SPLITBUTTON = 62, // Not defined in all oleacc.h version
+
+        /// <summary>
+        /// The object represents read-only text, such as labels for other controls or instructions in a dialog box. Static text cannot be modified or selected.
+        /// </summary>
+        ROLE_SYSTEM_STATICTEXT = 41,
+
+        /// <summary>
+        /// The object represents a status bar, which is an area at the bottom of a window and which displays information about the current operation, state of the application, or selected object. The status bar has multiple fields, which display different kinds of information.
+        /// </summary>
+        ROLE_SYSTEM_STATUSBAR = 23,
+
+        /// <summary>
+        /// The object represents a table that contains rows and columns of cells, and, optionally, row headers and column headers.
+        /// </summary>
+        ROLE_SYSTEM_TABLE = 24,
+
+        /// <summary>
+        /// The object represents selectable text that allows edits or is designated as read-only.
+        /// </summary>
+        ROLE_SYSTEM_TEXT = 42,
+
+        /// <summary>
+        /// The object represents a title or caption bar for a window.
+        /// </summary>
+        ROLE_SYSTEM_TITLEBAR = 1,
+
+        /// <summary>
+        /// The object represents a toolbar, which is a grouping of controls that provides easy access to frequently used features.
+        /// </summary>
+        ROLE_SYSTEM_TOOLBAR = 22,
+
+        /// <summary>
+        /// The object represents a tooltip that provides helpful hints.
+        /// </summary>
+        ROLE_SYSTEM_TOOLTIP = 13,
+
+        /// <summary>
+        /// The object represents blank space between other objects.
+        /// </summary>
+        ROLE_SYSTEM_WHITESPACE = 59,
+
+        /// <summary>
+        /// The object represents the window frame, which contains child objects such as a title bar, client, and other objects of a window.
+        /// </summary>
+        ROLE_SYSTEM_WINDOW = 9
+    }
 }
